ce91e47aebe2b5093c4426c8604bd028c6f1e11c
Radu <radutufilip@yahoo.com>
2014-11-24 09:22:23 +0200

    Icon change, CSS and behavioral adjustments

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+
+
 chrome.app.runtime.onLaunched.addListener(function() {
   runApp();
 });
--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-/**
- * Created by Radu on 3/1/14.
- */
- 
 
 // frame style
 var platform = navigator.platform;
 var winWidth = 940, winWidthCompact = 325,
-    winHeight = 500, winHeightCompact = 135;
+    winHeight = 500, winHeightCompact = 135,
+    prevLeft = 0, prevTop = 0;
 
 if (/mac/i.test(platform))
-    $(".frame").addClass("os-mac");
+    $("body").addClass("os-mac");
 else if (/linux/i.test(platform)) {
-    $(".frame").addClass("os-lnx");
+    $("body").addClass("os-lnx");
 } else {
-    $(".frame").addClass("os-win");
+    $("body").addClass("os-win");
 }
 
 
 chrome.app.window.current().resizeTo(winWidth, winHeight);
+
+
 
 // frame control buttons
 
@@ -25,17 +24,20 @@ var isMiniModeOn = false;
 var playMusicWV = document.querySelector('webview');
 
 $('#window-expand').hide();
+$('#window-restore').hide();
 
-$('#window-expand').click(function(e) { 
+$('#window-expand').click(function(e) {
     expandWindow();
     isMiniModeOn = false;
     chrome.app.window.current().resizeTo(winWidth, winHeight);
+    chrome.app.window.current().moveTo(prevLeft, prevTop);
 });
 
-$('#window-compact').click(function(e) { 
+$('#window-compact').click(function(e) {
     if (isMaximized()) {
         isMiniModeOn = true;
         restoreWindow();
+        minifyWindow();
     } else {
         minifyWindow();
     }
@@ -44,18 +46,17 @@ $('#window-compact').click(function(e) {
 $('#window-minimize').click(function(e) { chrome.app.window.current().minimize(); });
 $('#window-close').click(function(e) { window.close(); });
 
-$('#window-restore').hide();
 
-$('#window-maximize').click(function(e) { 
+$('#window-maximize').click(function(e) {
     $('#window-maximize').hide();
     $('#window-restore').show();
-    if (isMiniModeOn) { 
-        expandWindow(); 
+    if (isMiniModeOn) {
+        expandWindow();
     }
     chrome.app.window.current().maximize();
 });
 
-$('#window-restore').click(function(e) { 
+$('#window-restore').click(function(e) {
     restoreWindow();
     //chrome.app.window.current().resizeTo(winWidth, winHeight);
 });
@@ -70,6 +71,10 @@ function minifyWindow() {
     playMusicWV.insertCSS({file: "/css/gMusic_min.css", runAt: "document_start"});
     isMiniModeOn = true;
     chrome.app.window.current().resizeTo(winWidthCompact, winHeightCompact);
+
+    prevLeft = chrome.app.window.current().outerBounds.left;
+    prevTop = chrome.app.window.current().outerBounds.top;
+    chrome.app.window.current().moveTo(window.screen.availWidth,window.screen.availHeight);
 }
 
 
@@ -77,31 +82,36 @@ function expandWindow() {
     $('#window-expand').hide();
     $('#window-compact').show();
     playMusicWV.insertCSS({file: "/css/gMusic_max.css", runAt: "document_start"});
-
 };
 
-
-
 function restoreWindow() {
+    chrome.app.window.current().restore();
     $('#window-restore').hide();
     $('#window-maximize').show();
-    chrome.app.window.current().restore();
 }
 
 window.onresize = function(){
-    if (isMiniModeOn && chrome.app.window.current().outerBounds.width > 375 && !isMaximized()) {
+    if (isMiniModeOn && chrome.app.window.current().outerBounds.width > 375 && chrome.app.window.current().outerBounds.height > 175 && !isMaximized()) {
         expandWindow();
+        chrome.app.window.current().resizeTo(winWidth, winHeight);
         isMiniModeOn = false;
     }
 };
- 
+
 chrome.app.window.current().onRestored.addListener(function() {
     console.log("window restored");
-    // case when demaximize a previous mini mode 
+    // case when demaximize a previous mini mode
     // isMinimized check required to avoid the case in which restored is fired when minimizing from maximized state, clear as mud right?
-    if (isMiniModeOn && !isMaximized() && !isMinimized()) { 
+    if (isMiniModeOn && !isMaximized() && !isMinimized()) {
         minifyWindow();
     }
+});
+
+chrome.app.window.current().onMaximized.addListener(function() {
+  console.log("window maximized");
+  if (isMiniModeOn){
+      expandWindow();
+  }
 });
 
 
@@ -112,68 +122,21 @@ function isMaximized() {
 function isMinimized() {
     return chrome.app.window.current().isMinimized();
 }
- 
+
+
+
+
 /*
-var win_w_min = 325;
-var win_h_min = 135;
-var win_w_max = 940;
-var win_h_max = 500;
 
-window.onresize = doLayout;
-
-window.onload = function() {
-    var webview = document.querySelector('webview');
-
-    chrome.app.window.current().resizeTo(win_w_max, win_h_max);
-    doLayout();
-
-    document.querySelector('#b_min').onclick = function() {
-        webview.insertCSS({file: "/css/gMusic_min.css", runAt: "document_start"});
-        isMiniModeOn = true;
-        chrome.app.window.current().resizeTo(win_w_min, win_h_min);
-        document.getElementById("container").className = "min";
-
-        document.getElementById("switch").className = "test";
-
-        //console.log('gMusic MIN');
-    };
-
-    document.querySelector('#b_max').onclick = function() {
-        webview.insertCSS({file: "/css/gMusic_max.css", runAt: "document_start"});
-        isMiniModeOn = false;
-        chrome.app.window.current().resizeTo(win_w_max, win_h_max);
-        document.getElementById("container").className = "max";
-
-        //console.log('gMusic MAX');
-    };
-
-    chrome.app.window.current().onMaximized.addListener(function(){
-        if (isMiniModeOn){
-            webview.insertCSS({file: "/css/gMusic_max.css", runAt: "document_start"});
-            chrome.app.window.current().resizeTo(win_w_max, win_h_max);
-            isMiniModeOn = false;
-            document.getElementById("container").className = "max";
-
-            //console.log('Maximized from Mini Mode');
-
-        }
-    });
-
-    webview.addEventListener('contentload', function() {
-        webview.insertCSS({file: "/css/gMusic_max.css", runAt: "document_end"});
-    });
-}
-
-function doLayout() {
-
-    var webview = document.querySelector('webview');
-    if (isMiniModeOn){
-        webview.style.width = '' + win_w_min +'px';
-        webview.style.height = '' + win_h_min + 'px';
-    }else{
-        webview.style.width = (document.documentElement.clientWidth) + 'px';
-        webview.style.height = document.documentElement.clientHeight + 'px';
+$('#OSStyle').change(function(e) {
+    $("body").removeClass("os-lnx os-mac os-win");
+    if ($(this).val() == "mac")
+        $("body").addClass("os-mac");
+    else if ($(this).val() == "lnx") {
+        $("body").addClass("os-lnx");
+    } else {
+        $("body").addClass("os-win");
     }
 
-    //console.log('webview resized to '+webview.style.width+' x '+webview.style.height);
-}*/
+});*/
+
